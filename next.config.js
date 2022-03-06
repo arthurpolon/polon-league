e0b1c90a0a1a0c2c7a90e96bf31fdba93fa2b6bc
ARTHUR POLON <arthur.lemos@afya.com.br>
2022-03-06 12:46:11 -0400

    update next, add styled-components compiler option

--- next.config.js
+++ next.config.js
@@ -2,4 +2,8 @@
 module.exports = {
   reactStrictMode: true,
   pageExtensions: ['page.tsx', 'api.ts'],
+  compiler: {
+    // ssr and displayName are configured by default
+    styledComponents: true,
+  },
 }
